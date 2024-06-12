c59f3d27823fedc3ec319b69aedc5f2f59693ab3
KimDongSin <asstaff2020@gmail.com>
2024-06-13 02:43:47 +0900

    Style: loginStyled 폴더 분리

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,54 +1,7 @@
 import { useId } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-
-const Wrap = styled.div`
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Form = styled.form`
-  width: 400px;
-  background-color: white;
-  border-radius: 15px;
-  padding: 30px 40px;
-  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
-`;
-
-const Title = styled.h1`
-  font-size: 28px;
-  font-weight: bold;
-  text-align: center;
-  margin-bottom: 20px;
-  color: #333;
-`;
-
-const Label = styled.label`
-  display: block;
-  margin-bottom: 8px;
-  font-weight: bold;
-  color: #333;
-`;
-
-const Input = styled.input`
-  width: calc(100% - 20px);
-  padding: 10px;
-  margin-bottom: 20px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  outline: none;
-  &:focus {
-    border-color: #6e8efb;
-  }
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
+import { ButtonContainer, Form, FormInfoBox, Input, Label, Title, Wrap } from "../components/UI/styled/login.styled";
 
 const StyledLink = styled(Link)`
   width: 48%;
@@ -65,12 +18,6 @@ const StyledLink = styled(Link)`
   &:hover {
     background-color: #5a77d4;
   }
-`;
-
-const FormInfoBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
 `;
 
 export default function Login() {
