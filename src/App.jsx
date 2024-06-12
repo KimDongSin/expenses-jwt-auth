e8062b6c25e7d2a45815126fea6ce34d6a9ab2a0
KimDongSin <asstaff2020@gmail.com>
2024-06-13 00:06:32 +0900

    Feat: router분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home";
-import Detail from "./pages/Detail";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import MainRouter from "./router/MainRouter";
 
 function App() {
   return (
     <>
       <Provider store={store}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/detail/:id" element={<Detail />} />
-          </Routes>
-        </BrowserRouter>
+        <MainRouter />
       </Provider>
     </>
   );
